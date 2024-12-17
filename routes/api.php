72f6893939193e5e071751a99e830ab604f9957a
shaunan1 <eshaunans@gmail.com>
2024-12-17 09:45:53 +0700

    my1

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Http;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +20,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('categories', CategoryController::class);
+
+Route::get('/index', function () {
+    // URL API pihak ketiga
+    $url = 'https://api-tesingcat.vercel.app/api/categories';
+    
+    // Panggil API menggunakan Http::get()
+    $response = Http::get($url);
+
+    // Periksa jika request berhasil
+    if ($response->successful()) {
+        return response()->json($response->json(), 200);
+    }
+
+    // Handle jika terjadi kesalahan
+    return response()->json([
+        'error' => 'Unable to fetch data from external API',
+        'status' => $response->status()
+    ], $response->status());
+});
